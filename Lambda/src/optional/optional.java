c44761345f7e23a94402054fa2b51dfa1889b300
Biellms <biell.mendes8@gmail.com>
2022-07-04 16:49:53 -0300

    Creating Optional

--- Lambda/src/optional/optional.java
+++ Lambda/src/optional/optional.java
@@ -4,17 +4,27 @@ import java.util.Optional;
 
 public class optional {
     
+    /* OPTIONAL: classe que representa que um valor de retorno pode existir ou nao, sem ter que retornar null
+     * of -> retorna optional
+     * empty -> retorna optional vazio
+     * ofNullable -> permite retornar null
+     */
+
     public static void main(String[] args) {
         
-        String s = "113";
+        String s = "stringExample";
         Optional<Integer> numero = converteEmNumero(s);
         System.out.println(numero);
     }
 
     public static Optional<Integer> converteEmNumero(String string) {
 
-        Integer integer = Integer.valueOf(string);
+        try {
+            Integer integer = Integer.valueOf(string);
+            return Optional.ofNullable(integer);    
+        } catch (Exception e) {
+            return Optional.empty();
+        }
 
-        return Optional.of(integer);
     }
 }
