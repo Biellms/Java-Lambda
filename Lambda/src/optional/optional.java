e0508f548cb031c830cd5b10675d8e86a98a4ad3
Biellms <biell.mendes8@gmail.com>
2022-07-05 17:12:59 -0300

    Add Optional -> orElse/orElseGet/orElseThrow

--- Lambda/src/optional/optional.java
+++ Lambda/src/optional/optional.java
@@ -15,6 +15,9 @@ public class optional {
      * isPresent() -> verifica se há um valor presente ou nao
      * get() -> pega o elemento do Optinal. Caso esteja vazio ele retorna uma exeption
      * ifPresent() -> recebe uma lambda que so e executada se houver um valor presente dentro do Optional
+     * orElse() -> fornece um valor deafault a ser retornado
+     * orElseGet() -> igual o orElse, porem e retornado uma funcao lambda, o que torna a operecao mais complexa e custosa
+     * orElseThrow() -> lanca uma exception caso o Optional esteja vazio
      */
 
     public static void main(String[] args) {
@@ -22,17 +25,23 @@ public class optional {
         // String s = "8";
         String s = "stringExample";
         converteEmNumero(s)
-            .ifPresent(n -> System.out.println(n)); 
-        /* Optional<Integer> number = converEmNumero(number);
-         * System.out.println(number));
-         */
+            .ifPresent(n -> System.out.println("ifPresent: "+n));
+
+        Integer orElse = converteEmNumero(s)
+            .orElse(4);
+                System.out.println("orElse: "+orElse);
+
+
+        Integer orElseThrow = converteEmNumero(s)
+            .orElseThrow(() -> new NullPointerException("Optional vazio!"));
+                System.out.println("orElseThrow: "+orElseThrow);
     }
 
     public static Optional<Integer> converteEmNumero(String string) {
 
         try {
             Integer integer = Integer.valueOf(string);
-            return Optional.ofNullable(integer);    
+            return Optional.ofNullable(integer);
         } catch (Exception e) {
             return Optional.empty();
         }
