b2ad791f07342d0e8b576a24950bb878b91a6655
Biellms <biell.mendes8@gmail.com>
2022-07-09 00:24:52 -0300

    Add reduceExample

---
+++ Lambda/src/reduce/reduceExample.java
@@ -0,0 +1,23 @@
+package reduce;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class reduceExample {
+    
+    public static void main(String[] args) {
+        
+        /* REDUCE
+         * Pegar todos os elementos de uma stream e transforma-los em em um unico elemento. Sua função aplicada nos paramentro que dira qual sera sua forma de reduzir os elementos para apenas um.
+         */
+
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
+
+        // soma
+        Optional<Integer> soma = list.stream()
+            .reduce((n1, n2) -> n1 + n2);
+
+        System.out.println("Soma: "+soma);
+    }
+}
