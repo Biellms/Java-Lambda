b343d70bed5aea7680f2919948c17dd39abfbcf6
Biellms <biell.mendes8@gmail.com>
2022-07-09 16:45:18 -0300

    Add concatenação

--- Lambda/src/reduce/reduceExample.java
+++ Lambda/src/reduce/reduceExample.java
@@ -1,8 +1,6 @@
 package reduce;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 public class reduceExample {
     
@@ -13,6 +11,10 @@ public class reduceExample {
          */
 
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
+
+        String s = "Ola eu me chamo Gabriel e sou desenvolvedor!";
+        String[] split = s.split(" ");
+        List<String> listString = Arrays.asList(split);
 
         // soma
         Optional<Integer> soma = list.stream()
@@ -25,5 +27,17 @@ public class reduceExample {
             .reduce((n1, n2) -> n1 * n2);
         
         System.out.println("Multiplicacao: "+multi);
+
+        // concatenacao
+        Optional<String> concat = listString.stream()
+            .reduce((s1, s2) -> s1.concat(s2));
+
+        System.out.println("Concatenacao: "+concat);
+
+        // subtracao | NÃO FAÇA!
+        Optional<Integer> subtracao = list.stream()
+            .reduce((n1, n2) -> n1 - n2);
+
+        System.out.println("Subtracao: "+subtracao);
     }
 }
